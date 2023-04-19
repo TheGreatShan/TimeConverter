417e7a9237bd3cb6aea8213ffd00c72cded17e6b
smuraleetharan <smuraleetharan@kpmg.com>
2023-04-19 08:07:54 +0200

    :sparkles: Header has been created and globally added

---
+++ src/components/Header.vue
@@ -0,0 +1,18 @@
+<script lang="ts">
+export default {
+    props: {
+        title: String
+    }
+}
+</script>
+
+
+<template>
+    <div>
+        <header class="shadow bg-base-100">
+            <div class="max-w-7xl mx-auto py-6 px-4 sm:px-6 lg:px-8">
+                <h1 class="text-center text-3xl font-bold text-slate-100">{{ title }}</h1>
+            </div>
+        </header>
+    </div>
+</template>
--- src/main.ts
+++ src/main.ts
@@ -1,5 +1,9 @@
 import { createApp } from 'vue'
 import './style.css'
 import App from './App.vue'
+import MurasHeader from './components/Header.vue'
 
-createApp(App).mount('#app')
+
+const app = createApp(App)
+app.component('MurasHeader', MurasHeader)
+app.mount('#app')
