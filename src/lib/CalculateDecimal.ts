48e69b507bc25e427972ba6cd6206ab5ffba1496
smuraleetharan <smuraleetharan@kpmg.com>
2023-04-19 08:39:43 +0200

    :adhesive_bandage: Hours are rounded to 2 decimals

--- src/lib/CalculateDecimal.ts
+++ src/lib/CalculateDecimal.ts
@@ -1,14 +1,18 @@
-export function CalculateDecimal(hours : string) : number{
+export function CalculateDecimal(hours: string): number {
     const splittedHours: string[] = SplitHours(hours)
-    const minutes : number = parseFloat(splittedHours[1]) / 60
-    const hour : number = parseFloat(splittedHours[0])
-
-    return hour + minutes
+    const minutes: number = parseFloat(splittedHours[1]) / 60
+    const hour: number = parseFloat(splittedHours[0])
+    const roundedHours = RoundHours(hour + minutes)
+    return roundedHours 
 }
 
 
-function SplitHours(hours : string) : string[] {
+function SplitHours(hours: string): string[] {
     const splitted = hours.split(":")
-    
+
     return splitted
+}
+
+function RoundHours(decimal: number): number {        
+    return parseFloat(decimal.toFixed(2))
 }
