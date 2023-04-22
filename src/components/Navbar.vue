359bf2bbd0e721cd8a451fb635c6c4ff376cd7a5
shan15dev <shansai@bluewin.ch>
2023-04-22 13:10:53 +0200

    Navbar routes points to the homepage

--- src/components/Navbar.vue
+++ src/components/Navbar.vue
@@ -1,10 +1,10 @@
 <template>
-    <div className="navbar bg-base-100">
-        <div className="navbar-start">
-            <a className="btn btn-ghost normal-case text-xl">Muras</a>
+    <div class="navbar bg-base-100">
+        <div class="navbar-start">
+            <a class="btn btn-ghost normal-case text-xl" @click="$router.push({path: '/'})" >Muras</a>
         </div>
-        <div className="navbar-end">
-            <a className="btn" target="_blank" rel="noopener noreferrer" href="https://github.com/Shan15">Visit Github</a>
+        <div class="navbar-end">
+            <a class="btn" target="_blank" rel="noopener noreferrer" href="https://github.com/Shan15">Visit Github</a>
         </div>
     </div>
 </template>
