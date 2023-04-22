b708dcfb9b361cff6e1791957d7ca73533b048d2
shan15dev <shansai@bluewin.ch>
2023-04-22 18:46:25 +0200

    :pencil2: Page name has been modified in navbar

--- src/components/Navbar.vue
+++ src/components/Navbar.vue
@@ -1,7 +1,7 @@
 <template>
     <div class="navbar bg-base-100">
         <div class="navbar-start">
-            <a class="btn btn-ghost normal-case text-xl" @click="$router.push({path: '/'})" >Muras</a>
+            <a class="btn btn-ghost normal-case text-xl" @click="$router.push({path: '/'})" >Same same but different</a>
         </div>
         <div class="navbar-end">
             <a class="btn" target="_blank" rel="noopener noreferrer" href="https://github.com/Shan15">Visit Github</a>
